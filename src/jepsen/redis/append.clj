74115c33323ebe0d2955f3d8d332577bb6bb17af
Aphyr <aphyr@aphyr.com>
2020-03-09 14:41:14 -0400

    Append workload performs single ops outside txns

--- src/jepsen/redis/append.clj
+++ src/jepsen/redis/append.clj
@@ -12,6 +12,23 @@
             [jepsen.redis [client :as rc]]
             [taoensso.carmine :as car :refer [wcar]]))
 
+(defn apply-mop!
+  "Executes a micro-operation against a Carmine connection. This gets used in
+  two different ways. Executed directly, it returns a completed mop. In a txn
+  context, it's executed for SIDE effects, which must be reconstituted later."
+  [conn [f k v :as mop]]
+  (case f
+    :r      [f k (wcar conn (car/lrange k 0 -1))]
+    :append (do (wcar conn (car/rpush k (str v)))
+                mop)))
+
+(defn parse-read
+  "Turns reads of [:r :x ['1' '2'] into reads of [:r :x [1 2]]."
+  [[f k v :as mop]]
+  (case f
+    :r [f k (mapv parse-long v)]
+    :append mop))
+
 (defrecord Client [conn]
   client/Client
   (open! [this test node]
@@ -22,24 +39,24 @@
 
   (invoke! [_ test op]
     (rc/with-exceptions op #{}
-      (->> (:value op)
-           ; Perform micro-ops for side effects
-           (mapv (fn [[f k v]]
-                   (case f
-                     :r      (wcar conn (car/lrange k 0 -1))
-                     :append (wcar conn (car/rpush k (str v))))
-                   ; We force a small delay to widen the concurrency window
-                   ; here. Shouldn't matter, but it does break follower
-                   ; proxies.
-                   (Thread/sleep 500)))
-           ; In a transaction
-           (rc/with-txn conn)
-           ; And zip results back into the original txn
-           (mapv (fn [[f k v] r]
-                   [f k (case f
-                          :r      (mapv parse-long r)
-                          :append v)])
-                 (:value op))
+      (->> (if (< 1 (count (:value op)))
+             (->> (:value op)
+                  ; Perform micro-ops for side effects
+                  (mapv (partial apply-mop! conn))
+                  ; In a transaction
+                  (rc/with-txn conn)
+                  ; And zip results back into the original txn
+                  (mapv (fn [[f k v] r]
+                          [f k (case f
+                                 :r      r
+                                 :append v)])
+                        (:value op)))
+
+             ; Just execute the mop directly, without a txn
+             (->> (:value op)
+                  (mapv (partial apply-mop! conn))))
+           ; Parse integer reads
+           (mapv parse-read)
            ; Returning that completed txn as an OK op
            (assoc op :type :ok, :value))))
 
