7f8befef1db1bc7fb10c6b9c1053321dfb404c7a
Aphyr <aphyr@aphyr.com>
2020-03-27 15:35:09 -0400

    0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jepsen.redis "0.1.0-SNAPSHOT"
+(defproject jepsen.redis "0.1.0"
   :description "Jepsen tests for Redis"
   :url "https://github.com/jepsen-io/redis"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
